6db1eb5cc57cb4a6088df828ef2611c8fcc0b836
Sergey Kiselev <sergey@bitquery.io>
2024-03-07 12:50:52 +0000

    1.25.2 and compose

--- scripts/docker-lotus-entrypoint.sh
+++ scripts/docker-lotus-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -z $DOCKER_LOTUS_IMPORT_SNAPSHOT ]; then
+if [ ! -z $DOCKER_LOTUS_IMPORT_SNAPSHOT ] && ! $BACKFILL_MODE ; then
 	GATE="$LOTUS_PATH"/date_initialized
 	# Don't init if already initialized.
 	if [ ! -f "$GATE" ]; then
@@ -15,5 +15,64 @@ fi
 if [ ! -z $DOCKER_LOTUS_IMPORT_WALLET ]; then
 	/usr/local/bin/lotus-shed keyinfo import "$DOCKER_LOTUS_IMPORT_WALLET"
 fi
+
+if  $BACKFILL_MODE && [ ! -f "$GATE" ]; then
+     
+  todoarr=()
+
+  echo "rangestart is  $RANGESTART"
+  echo "rangeend is $RANGEEND"
+
+  for litelstitem in `curl https://forest-archive.chainsafe.dev/list/mainnet/lite | htmlq --attribute href a`
+  do
+    left=${litelstitem//*height_/}
+    height=${left//.forest*/}
+    lites[$height]=$litelstitem
+  done
+
+  for difflstitem in `curl https://forest-archive.chainsafe.dev/list/mainnet/diff | htmlq --attribute href a`
+  do
+    left=${difflstitem//*height_/}
+    height=${left//.forest*/}
+    diffs[$height]=$difflstitem
+  done
+
+  let liteindex=$RANGESTART/30000*30000 
+
+  todoarr+=("${lites[$liteindex]}")
+
+  diffindex=$liteindex
+
+  while [ $diffindex -lt $RANGEEND ]
+  do
+   todoarr+=("${diffs[$diffindex]}")
+   let diffindex=$diffindex+3000
+  done
+
+  todoarr+=("https://forest-archive.chainsafe.dev/latest/mainnet/")
+
+  mkdir /var/lib/lotus/process
+  cd /var/lib/lotus/process
+
+  for value in "${todoarr[@]}"
+  do
+    echo "==================================    Processing $value"
+    echo
+    wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 0 -q -c $value -O processfile.zst
+    zstd -d processfile.zst
+    rm -rf processfile.zst
+    /usr/local/bin/lotus daemon --halt-after-import --import-snapshot processfile
+    rm -rf processfile
+    echo "------------------------- done $value"
+    echo
+    echo
+    sleep 3 
+  done
+
+  date > "$GATE" 
+
+fi
+
+cd /
 
 exec /usr/local/bin/lotus $@
